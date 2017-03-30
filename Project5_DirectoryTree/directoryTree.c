e92ce6ab1422566e131af1a0a6a3fddd2f044098
Ryan Scopio <ryanscopio@gmail.com>
2017-03-30 15:00:47 -0700

    added example code for project 5

---
+++ Project5_DirectoryTree/directoryTree.c
@@ -0,0 +1,56 @@
+#include <dirent.h>
+#include <stdio.h>
+
+int my_double_ls(const char *name)
+{
+    struct dirent *d;
+    DIR *dp;
+
+    /* open the directory and check for failure */
+    if ((dp = opendir(name)) == NULL)
+    {
+        return (-1);
+    }
+
+    /* continue looping through the directory
+     * printing out the directory entry name as long
+     * as the inode number is valid
+     */
+    while (d = readdir(dp))
+    {
+        if (d->d_ino != 0)
+        {
+            printf("%s\n", d->d_name);
+        }
+    }
+
+    /* now go back to the beginning of the directory ... */
+    rewinddir(dp);
+
+    /* ... and print out the directory again */
+    while (d = readdir(dp))
+    {
+        if (d->d_ino != 0)
+        {
+            printf("%s\n", d->d_name);
+        }
+    }
+
+    closedir(dp);
+    return (0);
+}
+
+int main()
+{
+    int errors;
+    errors = 0;
+
+    if (errors)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
