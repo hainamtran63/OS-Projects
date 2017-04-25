f2321d0e3e7873ecd2a4c46f39c8cdb4580d133a
Ryan Scopio <ryanscopio@gmail.com>
2017-04-25 15:56:11 -0700

    Changed bash script to use find instead, this is dumb

--- Project5_DirectoryTree/dtScript.sh
+++ Project5_DirectoryTree/dtScript.sh
@@ -1,47 +1,5 @@
 #!/bin/bash
 
-# file, indent value
-print_file() {
-    word=""
-    COUNTER=0
-    while [  $COUNTER -lt $2 ]; do
-        word+="-"
-        let COUNTER=COUNTER+1 
-    done
-    echo $word$1
-}
+#ls -R -l $1 | grep '^d' | awk '{print $9}' | sort
 
-# dir, indent value
-print_dir () {
-
-    print_file $1 $2
-
-    for file in $(ls "$1")
-    do
-        if [ -d ${1}/${file} ] ; then
-            let num=$2+1
-            print_dir "${1}/${file}" $num
-        fi
-    done
-}
-
-display_dir() {
-    let level=$2+1
-    row=1
-    for file in $(ls "$1")
-    do
-        
-
-        let line=$row
-        line+="p"
-        echo info:$row, $line, $1
-        ls -l $1 | grep '^d' | awk '{print $9}' | sed -n $line
-        display_dir "${1}/${file}" $level
-        let row+=1
-    done
-}
-
-#ls -l $1 | grep '^d' | awk '{print $9}' | sed -n 1p
-
-echo $1
-display_dir $1 0
+find $1 -type d -print
