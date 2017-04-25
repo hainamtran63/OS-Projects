3426906e5c140938d2286ae183e2ab613d4dfb91
Ryan Scopio <ryanscopio@gmail.com>
2017-04-25 14:39:22 -0700

    Added HW3, Renamed folders & continued to work on Project 5 script

--- Project5_DirectoryTree/dtScript.sh
+++ Project5_DirectoryTree/dtScript.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
-recurse_dir(){
-    for file in $1
-    do
-        if [ -d $file ] ; then
-            let level=$4+1
-            print_dir $1 $file $root $level
-        fi
-    done 
+# file, indent value
+print_file() {
+    word=""
+    COUNTER=0
+    while [  $COUNTER -lt $2 ]; do
+        word+="-"
+        let COUNTER=COUNTER+1 
+    done
+    echo $word$1
 }
 
-# rootFile, root#
+# dir, indent value
 print_dir () {
-    let root=$2+1
-    let arg=$root
-    arg+="p"
-    ls -l $1 | grep '^d' | awk '{print $9}' | sed -n $arg
-    #recurse for current root
-    for file in $1
+
+    print_file $1 $2
+
+    for file in $(ls "$1")
     do
-        if [ -d $file ] ; then
-            
-            print_dir $1 $root
+        if [ -d ${1}/${file} ] ; then
+            let num=$2+1
+            print_dir "${1}/${file}" $num
         fi
     done
 }
 
-print_dir $1 0
+display_dir() {
+    let level=$2+1
+    row=1
+    for file in $(ls "$1")
+    do
+        
+
+        let line=$row
+        line+="p"
+        echo info:$row, $line, $1
+        ls -l $1 | grep '^d' | awk '{print $9}' | sed -n $line
+        display_dir "${1}/${file}" $level
+        let row+=1
+    done
+}
+
+#ls -l $1 | grep '^d' | awk '{print $9}' | sed -n 1p
+
+echo $1
+display_dir $1 0
